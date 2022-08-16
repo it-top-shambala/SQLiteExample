ae14aa67c1c631e7cfc2261f903115851831ea6d
Starinin Andrey <starininandrey@gmail.com>
2022-08-16 18:12:52 +0300

    Add model

---
+++ SQLiteExample.Model/Person.cs
@@ -0,0 +1,13 @@
+﻿namespace SQLiteExample.Model;
+
+public class Person
+{
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+
+    public string? FullName => $"{this.LastName} {this.FirstName}";
+
+    public DateTime DateOfBirth { get; set; }
+
+    public Dictionary<string, string>? Contacts { get; set; }
+}
