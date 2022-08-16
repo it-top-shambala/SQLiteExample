7f1d4e6d217bc1ed28598f668baa9720947d0d88
Starinin Andrey <starininandrey@gmail.com>
2022-08-16 18:12:41 +0300

    Reformat

--- SQLiteExample.DbLib/DataBase.cs
+++ SQLiteExample.DbLib/DataBase.cs
@@ -1,6 +1,13 @@
 ﻿namespace SQLiteExample.DbLib;
 
+using Microsoft.Data.Sqlite;
+
 public class DataBase
 {
+    private readonly SqliteConnection db;
+    private const string Str = @"Data Source=D:\Programming\Education\ITStep\Shambala\SQLiteDemo\persons.db;";
+
+    public DataBase() => this.db = new SqliteConnection(Str);
+
     
 }
